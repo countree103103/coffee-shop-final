da343d9783acfa2fd3b8921f22d56c9414e1f8d7
Countree <717662837@qq.com>
2021-12-14 11:46:30 +0800

    引入axios

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import "./plugins/axios";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { gsap } from "gsap";
 
