72760a993aff0243758e17621a9d3e9d07c46d31
Countree <717662837@qq.com>
2021-12-14 15:47:35 +0800

    添加头像工具类

---
+++ src/utils/index.js
@@ -0,0 +1,26 @@
+function reader(file, options) {
+  options = options || {};
+  return new Promise(function (resolve, reject) {
+    let reader = new FileReader();
+
+    reader.onload = function () {
+      resolve(reader);
+    };
+    reader.onerror = reject;
+
+    if (options.accept && !new RegExp(options.accept).test(file.type)) {
+      reject({
+        code: 1,
+        msg: "wrong file type",
+      });
+    }
+
+    if (!file.type || /^text\//i.test(file.type)) {
+      reader.readAsText(file);
+    } else {
+      reader.readAsDataURL(file);
+    }
+  });
+}
+
+export { reader };
