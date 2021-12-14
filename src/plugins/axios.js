a097feab4df15c4d20c84ce51fe678649f8a3c3f
Countree <717662837@qq.com>
2021-12-14 15:11:50 +0800

    更改axios配置

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,6 +2,7 @@
 
 import Vue from "vue";
 import axios from "axios";
+import { SERVER_ADDRESS } from "../config";
 
 // Full config:  https://github.com/axios/axios#request-config
 // axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
@@ -10,52 +11,12 @@ import axios from "axios";
 
 let config = {
   // baseURL: process.env.baseURL || process.env.apiUrl || ""
+  baseURL: SERVER_ADDRESS,
   // timeout: 60 * 1000, // Timeout
-  // withCredentials: true, // Check cross-site Access-Control
+  withCredentials: true, // Check cross-site Access-Control
 };
 
-const _axios = axios.create(config);
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = SERVER_ADDRESS;
 
-_axios.interceptors.request.use(
-  function (config) {
-    // Do something before request is sent
-    return config;
-  },
-  function (error) {
-    // Do something with request error
-    return Promise.reject(error);
-  }
-);
-
-// Add a response interceptor
-_axios.interceptors.response.use(
-  function (response) {
-    // Do something with response data
-    return response;
-  },
-  function (error) {
-    // Do something with response error
-    return Promise.reject(error);
-  }
-);
-
-Plugin.install = function (Vue, options) {
-  Vue.axios = _axios;
-  window.axios = _axios;
-  Object.defineProperties(Vue.prototype, {
-    axios: {
-      get() {
-        return _axios;
-      },
-    },
-    $axios: {
-      get() {
-        return _axios;
-      },
-    },
-  });
-};
-
-Vue.use(Plugin);
-
-export default Plugin;
+export default axios;
